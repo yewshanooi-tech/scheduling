dfc3be9c995e9b67f1a36b1b0842bcd87a1d4be5
Ooi Yew Shan <yewshanooi@bloomthis.co>
2025-05-29 12:02:22 +0800

    fix: renamed hello_world folder to scheduling

--- src/scheduling/constraints.py
+++ src/scheduling/constraints.py
@@ -1,6 +1,7 @@
 from timefold.solver.score import (constraint_provider, HardSoftScore, Joiners,
                                    ConstraintFactory, Constraint, ConstraintCollectors)
 # from datetime import time
+from typing import Optional
 
 from .domain import Assignment
 
@@ -11,16 +12,31 @@ def define_constraints(constraint_factory: ConstraintFactory):
     return [
         # Hard constraints
         florist_conflict(constraint_factory),
-        team_conflict(constraint_factory)
+        team_conflict(constraint_factory),
+        no_overlapping_shifts(constraint_factory)
     ]
+
 
 
 def get_shift(a: Assignment) -> object:
     return a.shift
 
+def get_shift_start(a: Assignment) -> Optional[int]:
+    if a.shift is not None and a.shift.start_time is not None:
+        return a.shift.start_time.hour * 60 + a.shift.start_time.minute
+    return None
+
+def get_shift_end(a: Assignment) -> Optional[int]:
+    if a.shift is not None and a.shift.end_time is not None:
+        return a.shift.end_time.hour * 60 + a.shift.end_time.minute
+    return None
+
 
 def get_florist(a: Assignment) -> object:
     return a.florist
+
+def get_florist_name(a: Assignment) -> object:
+    return a.florist.name
 
 
 
@@ -42,5 +58,15 @@ def team_conflict(constraint_factory: ConstraintFactory) -> Constraint:
             .filter(lambda _, count: count > 4)
             .penalize(HardSoftScore.ONE_HARD, lambda _, count: count - 4)
             .as_constraint("Team capacity conflict"))
+
+
+# A florist cannot be assigned to overlapping shifts.
+def no_overlapping_shifts(constraint_factory: ConstraintFactory) -> Constraint:
+    return (constraint_factory
+            .for_each_unique_pair(Assignment,
+                                  Joiners.equal(get_florist_name),
+                                  Joiners.overlapping(get_shift_start, get_shift_end))
+            .penalize(HardSoftScore.ONE_HARD, lambda a1, a2: 1)
+            .as_constraint("Overlapping shift"))
 
 
